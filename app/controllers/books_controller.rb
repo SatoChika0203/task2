aa658d62b77e7caa44acfb26a525f2dd951b2140
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-04-14 12:12:44 +0000

    [Add]削除機能の追加、フラッシュメッセージの追加、ルートの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,14 @@ class BooksController < ApplicationController
 
   def create
    @book=Book.new(book_params)
-   @book.save
-   redirect_to 'top'
+  if @book.save
+   flash[:notice]="Book was successfully created."
+   redirect_to book_path(@book.id)
+  else
+  @books=Book.all 
+  # renderしたindex.htmlはcreateアクションから呼び出されるため、createにも@Books作る
+   render :index
+  end
   end
 
   def index
@@ -20,16 +26,24 @@ class BooksController < ApplicationController
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def update
     book=Book.find(params[:id])
     book.update(book_params)
-    redirect_to show_book_path(book.id)
+    flash[:notice]="Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
   end
 
 private
 def book_params
   params.require(:book).permit(:title, :body)
-  
+
 end
 end
