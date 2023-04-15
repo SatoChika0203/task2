81fd5cbeff70ad8665c4d2a766ae5f56a3f50eae
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-04-15 23:38:52 +0000

    [Finish]アプリケーションを完成させよう」

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,14 @@
 class BooksController < ApplicationController
-  def new
-  end
-
+  
   def create
    @book=Book.new(book_params)
   if @book.save
    flash[:notice]="Book was successfully created."
    redirect_to book_path(@book.id)
   else
-  @books=Book.all 
+    @books=Book.all 
   # renderしたindex.htmlはcreateアクションから呼び出されるため、createにも@Books作る
-   render :index
+    render :index
   end
   end
 
@@ -28,10 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice]="Book was successfully updated."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
   end
 
   def destroy
@@ -44,6 +45,6 @@ class BooksController < ApplicationController
 private
 def book_params
   params.require(:book).permit(:title, :body)
-
 end
+
 end
